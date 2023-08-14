3769128b4b3b7595ae9e4f30a65302158a8f2432
GIOVANNA <141193044+Giovanna1205@users.noreply.github.com>
2023-08-14 20:19:05 -0300

    Add files via upload

---
+++ Projetos/src/Aula05/src/aula05/Aula5.java
@@ -0,0 +1,29 @@
+
+package aula05;
+
+/**
+ *
+ * @author giovanna
+ */
+public class Aula5 {
+    
+
+public static void main (String[]args) {
+int a=10;
+String b="10";
+int c=2;
+String d="10";
+int resultadoA=a+c;
+int resultadoB=a-c;
+int resultadoC=a*c;
+int resultadoD=a/c;
+int resultadoE=a%c;
+int resultadoF=(int)a/c;
+System.out.println(resultadoA);
+System.out.println(resultadoB);
+System.out.println(resultadoC);
+System.out.println(resultadoD);
+System.out.println(resultadoE);
+System.out.println(resultadoF);
+}
+}

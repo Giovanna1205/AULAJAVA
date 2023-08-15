d271444bb2240ac2b8b5e64a51a366002659c9a8
17932453631 <17932453631@BP-LABINF07.fha.local>
2023-08-15 18:52:26 -0300

    aulas,1,2,3,4,5

---
+++ Aula04/src/aula04/Aula04.java
@@ -0,0 +1,24 @@
+
+package aula04;
+
+/**
+ *
+ * @author giovanna
+ */
+public class Aula04 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("O valor de pi é:"+ Math.PI);
+        System.out.println("O máximo é:"+ Math.max(5,10));
+        System.out.println("O mínimo é:"+ Math.min(5,10));
+        System.out.println("O raiz quadrada é:"+ Math.sqrt(64));
+        
+       
+        System.out.println("Exponencial e potenciação \n");
+        System.out.println("O valor de E é:"+ Math.E+ "\n\n");
+        System.out.println("'e' elevado ao quadrado="+ Math.exp(2));
+        System.out.println("2 elevado"+ Math.pow(2,3));
+    }
+    
+}

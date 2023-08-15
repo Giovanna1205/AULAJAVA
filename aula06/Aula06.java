d269da492300b9138671eb903158f7335cba50e8
GIOVANNA <141193044+Giovanna1205@users.noreply.github.com>
2023-08-15 19:21:24 -0300

    Add files via upload

---
+++ aula06/Aula06.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula06;
+
+/**
+ *
+ * @author giovanna
+ */
+public class Aula06 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float n1=5,n2=8,n3=10,n4=9;
+        float soma=n1+n2+n3+n4;
+        float media=soma/4;
+        System.out.print ("A média entre as notas é:"+media);
+                
+      
+        
+        
+        
+        
+        
+        
+    }
+        
+    }
+    
+}

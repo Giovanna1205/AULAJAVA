b834291350218450b9af6df7e91a62561406950a
17932453631 <17932453631@BP-LABINF07.fha.local>
2023-08-15 21:33:52 -0300

    aula 07

---
+++ Aula08/src/aula08/Aula08.java
@@ -0,0 +1,27 @@
+
+package aula08;
+
+/**
+ *
+ * @author Giovanna
+ */
+public class Aula08 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        System.out.println("a > b && a < b");
+        if (a > b) {
+            System.out.println("Maior");
+        }else if{
+            System.out.println("Menor");
+            }else{
+            System.out.println("igual");
+        }
+        
+    }
+    
+}

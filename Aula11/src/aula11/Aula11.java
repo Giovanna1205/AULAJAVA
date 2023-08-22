6bb449e624bca7df056ba6f38b7c89454115d67e
17932453631 <17932453631@BP-LABINF07.fha.local>
2023-08-22 18:51:06 -0300

    aula 11

---
+++ Aula11/src/aula11/Aula11.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula11;
+
+/**
+ *
+ * @author 17932453631
+ */
+public class Aula11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int contador=0;
+        while (contador < 10){
+            System.out.println("contador");
+            contador++;
+        }
+        for (int i = 0;i < 10; i++){
+        System.out.println(i);
+    }
+    }
+    
+}

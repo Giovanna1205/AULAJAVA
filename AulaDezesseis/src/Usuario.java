f49fda20ee9d1bc1337ec6cb3672533abf40257b
GIOVANNA <141193044+Giovanna1205@users.noreply.github.com>
2023-09-01 19:50:05 -0300

    Add files via upload

---
+++ AulaDezesseis/src/Usuario.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 17932453631
+ */
+public class Usuario {
+    private String usuario="giovanna";
+            private String senha="123";
+            
+            public String getUsuario() {
+                return usuario;
+            }
+    public void setUsuario(String usuario){
+        this.usuario=usuario;
+    }
+    public String getSenha(){
+        return senha;
+    }
+    public void setSenha(String senha){
+        this.senha=senha;
+    }
+}

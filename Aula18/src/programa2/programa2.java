f85116a8ae24ddce13bb0bf23bfb90ef6c932b79
GIOVANNA <141193044+Giovanna1205@users.noreply.github.com>
2023-09-05 20:13:11 -0300

    Add files via upload

---
+++ Aula18/src/programa2/programa2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programa2;
+
+/**
+ *
+ * @author 17932453631
+ */
+public class programa2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int incr = 1;
+        int decr = 15;
+        
+        for (int i = 0;i < 15; i++){
+            System.out.println(incr+"/"+decr);
+            incr++;
+            decr--;
+        }
+    }
+    
+}
